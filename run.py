fb7d4b560c90963e3b1cfd7e09dc9ab45efb628c
Avinash Sidhu <avinashsidhu@MacBook-Air.local>
2023-09-06 15:07:38 -0400

    Latest changes to fetch required info

--- run.py
+++ run.py
@@ -14,24 +14,44 @@ def main():
     """
     Entry point to run the application
     """
-    # parser = argparse.ArgumentParser(description='Run the Job.')
-    # parser.add_argument('config', help='A configuration file in YAML format.')
-    # args = parser.parse_args()
-    # config = yaml.safe_load(open(args.config, 'rt', encoding='utf8'))
-    config_path = f'{os.getcwd()}/configs/config.yml'
-    config = yaml.safe_load(open(config_path, 'rt', encoding='utf8'))
+    parser = argparse.ArgumentParser(description='Run the Job.')
+    parser.add_argument('config', help='A configuration file in YAML format.')
+    args = parser.parse_args()
+    config = yaml.safe_load(open(args.config, 'rt', encoding='utf8'))
+    # config_path = f'{os.getcwd()}/configs/config.yml'
+    # config = yaml.safe_load(open(config_path, 'rt', encoding='utf8'))
     log_config = config['logging']
+    run_config = config['run']
+    s3_config = config['s3']
     logging.config.dictConfig(log_config)
 
     logger = logging.getLogger(__name__)
     logger.info('Job started.')
-    source_api = SourceConnector('API_KEY')
-    destination_db = DestinationConnector()
-    pipeline = InstagramPipeline(source_api, destination_db)
-    user_id = 123
-    pipeline.ETL_per_user(user_id, datetime.strptime("2023-1-1", "%Y-%m-%d"))
-    logger.info('Job finished.')
 
+    source_api = SourceConnector('API_KEY')
+    destination_db = DestinationConnector(access_key=s3_config['access_key'],
+                                      secret_key=s3_config['secret_key'],
+                                      endpoint_url=s3_config['endpoint_url'],
+                                      bucket=s3_config['bucket'])
+
+    if run_config['type'] == 'POSTS':
+        logger.info('Fetching post info')
+        pipeline = InstagramPipeline(source_api, destination_db)
+        path_to_shortcodes = run_config['path']
+        column_name = run_config['column_name']
+        pipeline.ETL_posts(path_to_shortcodes, column_name)
+    elif run_config['type'] == 'USERS':
+        logger.info('Fetching user info.')
+        pipeline = InstagramPipeline(source_api, destination_db)
+        path_to_user_ids = run_config['path']
+        time_to_process_from = run_config['time']
+        time_format = run_config['time_format']
+        column_name = run_config['column_name']
+        pipeline.ETL_users(path_to_user_ids, column_name, time_to_process_from, time_format)
+    else:
+        logger.info('Please provide correct run type.')
+    logger.info('Job finished.')
+    logging.shutdown()
 
 if __name__ == '__main__':
     main()
--- source/connector.py
+++ source/connector.py
@@ -5,8 +5,9 @@ import os
 import json
 import logging
 import requests
-import pymongo
-from source.constants import APIConstants, PipelineConstants, DBConstants
+# import pymongo
+import boto3
+from source.constants import APIConstants, PipelineConstants
 
 
 class SourceConnector():
@@ -54,21 +55,54 @@ class DestinationConnector():
     """
     Class for the conenctor methods.
     """
-    def __init__(self):
+    # def __init__(self):
+    #     """
+    #     Constructor for the Connector class
+    #     """
+    #     self._logger = logging.getLogger(__name__)
+    #     self._client = pymongo.MongoClient(DBConstants.HOST_URL.value)
+    #     self._db = self._client[DBConstants.DB_NAME.value]
+    def __init__(self, access_key: str, secret_key: str, endpoint_url: str, bucket: str):
         """
-        Constructor for the Connector class
+        Constructor for S3BucketConnector
+
+        :param access_key: access key for accessing S3
+        :param secret_key: secret key for accessing S3
+        :param endpoint_url: endpoint url to S3
+        :param bucket: S3 bucket name
         """
         self._logger = logging.getLogger(__name__)
-        self._client = pymongo.MongoClient(DBConstants.HOST_URL.value)
-        self._db = self._client[DBConstants.DB_NAME.value]
-
-    def insert_into_collection(self, collection_name: str, **kwargs):
+        self.endpoint_url = endpoint_url
+        self._s3 = boto3.resource('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+        self._bucket = bucket
+    
+    def write_to_s3(self, storing_key: str, **kwargs):
         """
-        Store json data into the MongoDB database
+        writing pandas dataframe to s3
+        supported formats: csv, parquet
 
-        :param collection_name: Name of collection where the data is to be stored
+        :param dataframe: Pandas dataframe to be written
+        :param key: key to the saved file
+        :para file_format: format of the file to be saved
         """
-        collection  = self._db[collection_name]
+        self._logger.info('Writing to s3 with key %s', storing_key)
         data = {key: value for key, value in kwargs.items()}
-        collection.insert_one(data)
+        # s3_object = self._s3.Object(self._bucket, storing_key)
+        # s3_object.put(
+        #     Body=(bytes(json.dumps(data).encode('UTF-8')))
+        # )
+        with open(f'{os.getcwd()}/{storing_key}', "w", encoding = 'UTF-8') as outfile:
+            outfile.write(json.dumps(data, indent = 6))
+        self._logger.info('Written to s3 with key %s', storing_key)
         return True
+    
+    # def insert_into_collection(self, collection_name: str, **kwargs):
+    #     """
+    #     Store json data into the MongoDB database
+
+    #     :param collection_name: Name of collection where the data is to be stored
+    #     """
+    #     collection  = self._db[collection_name]
+    #     data = {key: value for key, value in kwargs.items()}
+    #     collection.insert_one(data)
+    #     return True
